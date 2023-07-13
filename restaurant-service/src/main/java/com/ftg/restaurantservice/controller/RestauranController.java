ffefc2176cd5441d15b201da9ec0905ed6e1df14
sk obedullah <obedullahsk@gmail.com>
2023-07-13 21:47:41 +0530

    added cross origin in restaurant controller

--- restaurant-service/src/main/java/com/ftg/restaurantservice/controller/RestauranController.java
+++ restaurant-service/src/main/java/com/ftg/restaurantservice/controller/RestauranController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("api/restaurant-service")
 @AllArgsConstructor
+@CrossOrigin(originPatterns = "*")
 public class RestauranController {
 
 	private final Logger logger = LoggerFactory.getLogger(RestauranController.class);
